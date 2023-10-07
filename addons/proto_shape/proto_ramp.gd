fd3ef800056fc15c6875afaadfe16be966a6c6b1
HLCaptain <pkblazsak@gmail.com>
2023-10-07 12:35:37 +0200

    Added anchor for ramp. Anchor can be set to different parts of the ramp to help adjusting its position.

--- addons/proto_shape/proto_ramp.gd
+++ addons/proto_shape/proto_ramp.gd
@@ -6,6 +6,18 @@ enum Calculation {
 	STEP_DIMENSIONS,
 }
 
+enum Anchor {
+	BOTTOM_CENTER,
+	BOTTOM_LEFT,
+	BOTTOM_RIGHT,
+	TOP_CENTER,
+	TOP_LEFT,
+	TOP_RIGHT,
+	BASE_CENTER,
+	BASE_LEFT,
+	BASE_RIGHT,
+}
+
 enum Type {
 	RAMP,
 	STAIRCASE,
@@ -19,22 +31,19 @@ var _height = 1.0
 var _depth = 1.0
 var _fill = true
 var _type = Type.RAMP
+var _anchor = Anchor.BOTTOM_CENTER
+var _anchor_fixed = true
 
 @export_category("Proto Stairs")
-var calculation : Calculation : set = set_calculation, get = get_calculation
-var steps : int : set = set_steps, get = get_steps
-var width : float : set = set_width, get = get_width
-var height : float : set = set_height, get = get_height
-var depth : float : set = set_depth, get = get_depth
-var fill : bool : set = set_fill, get = get_fill
-var type : Type : set = set_type, get = get_type
-
-# @export var calculation : Calculation : set = set_calculation, get = get_calculation
-# @export_range(1, 100, 1, "or_greater") var steps : int : set = set_steps, get = get_steps
-# @export_range(0.001, 100, 0.01, "or_greater") var width : float : set = set_width, get = get_width
-# @export_range(0.001, 100, 0.01, "or_greater") var height : float : set = set_height, get = get_height
-# @export_range(0.001, 100, 0.01, "or_greater") var depth : float : set = set_depth, get = get_depth
-# @export var fill : bool : set = set_fill, get = get_fill
+var calculation: Calculation: set = set_calculation, get = get_calculation
+var steps: int: set = set_steps, get = get_steps
+var width: float: set = set_width, get = get_width
+var height: float: set = set_height, get = get_height
+var depth: float: set = set_depth, get = get_depth
+var fill: bool: set = set_fill, get = get_fill
+var type: Type: set = set_type, get = get_type
+var anchor: Anchor: set = set_anchor, get = get_anchor
+var anchor_fixed: bool: set = set_anchor_fixed, get = get_anchor_fixed
 
 func _get_property_list():
 	var list = []
@@ -42,6 +51,8 @@ func _get_property_list():
 	list.append({"name": "width", "type": TYPE_FLOAT, "hint": PROPERTY_HINT_RANGE, "hint_string": "0.001,100,0.01,or_greater"})
 	list.append({"name": "height", "type": TYPE_FLOAT, "hint": PROPERTY_HINT_RANGE, "hint_string": "0.001,100,0.01,or_greater"})
 	list.append({"name": "depth", "type": TYPE_FLOAT, "hint": PROPERTY_HINT_RANGE, "hint_string": "0.001,100,0.01,or_greater"})
+	list.append({"name": "anchor", "type": TYPE_INT, "hint": PROPERTY_HINT_ENUM, "hint_string": "Bottom Center,Bottom Left,Bottom Right,Top Center,Top Left,Top Right,Base Center,Base Left,Base Right"})
+	list.append({"name": "anchor_fixed", "type": TYPE_BOOL})
 
 	# Staircase exclusive properties
 	if type == Type.STAIRCASE:
@@ -67,6 +78,10 @@ func _set(property, value):
 			set_depth(value)
 		"fill":
 			set_fill(value)
+		"anchor":
+			set_anchor(value)
+		"anchor_fixed":
+			set_anchor_fixed(value)
 
 func get_type():
 	return _type
@@ -88,6 +103,67 @@ func get_fill():
 
 func get_steps():
 	return _steps
+
+func get_anchor():
+	return _anchor
+
+func get_anchor_fixed():
+	return _anchor_fixed
+
+func get_true_step_depth():
+	var step_calculation = calculation
+	var step_depth: float
+	if type == Type.RAMP:
+		step_calculation = Calculation.STEP_DIMENSIONS
+	match step_calculation:
+		Calculation.STAIRCASE_DIMENSIONS:
+			step_depth = depth / steps
+		Calculation.STEP_DIMENSIONS:
+			step_depth = depth
+	return step_depth
+
+func get_true_depth():
+	return get_true_step_depth() * steps
+
+func get_true_step_height():
+	var step_calculation = calculation
+	var step_height: float
+	if type == Type.RAMP:
+		step_calculation = Calculation.STEP_DIMENSIONS
+	match step_calculation:
+		Calculation.STAIRCASE_DIMENSIONS:
+			step_height = height / steps
+		Calculation.STEP_DIMENSIONS:
+			step_height = height
+	return step_height
+
+func get_true_height():
+	return get_true_step_height() * steps
+
+func get_anchor_offset(anchor):
+	var offset = Vector3()
+	var depth = get_true_depth()
+	var height = get_true_height()
+	match anchor:
+		Anchor.BOTTOM_CENTER:
+			offset = Vector3(0, 0, 0)
+		Anchor.BOTTOM_LEFT:
+			offset = Vector3(-width / 2.0, 0, 0)
+		Anchor.BOTTOM_RIGHT:
+			offset = Vector3(width / 2.0, 0, 0)
+		Anchor.TOP_CENTER:
+			offset = Vector3(0, -height, -depth)
+		Anchor.TOP_LEFT:
+			offset = Vector3(-width / 2.0, -height, -depth)
+		Anchor.TOP_RIGHT:
+			offset = Vector3(width / 2.0, -height, -depth)
+		Anchor.BASE_CENTER:
+			offset = Vector3(0, 0, -depth)
+		Anchor.BASE_LEFT:
+			offset = Vector3(-width / 2.0, 0, -depth)
+		Anchor.BASE_RIGHT:
+			offset = Vector3(width / 2.0, 0, -depth)
+	return offset
 
 func set_type(value):
 	_type = value
@@ -158,6 +234,20 @@ func set_fill(value):
 	for child in get_children():
 		refresh_step(child.get_index())
 
+func set_anchor(value):
+	# Transform node to new anchor
+	translate_anchor(anchor, value)
+	_anchor = value
+	for child in get_children():
+		refresh_step(child.get_index())
+
+func translate_anchor(from_anchor, to_anchor):
+	if !anchor_fixed:
+		translate_object_local(get_anchor_offset(from_anchor) - get_anchor_offset(to_anchor))
+
+func set_anchor_fixed(value):
+	_anchor_fixed = value
+
 func set_steps(value):
 	_steps = value
 	refresh_steps(value)
@@ -186,21 +276,11 @@ func refresh_steps(new_steps):
 
 func refresh_step(i: int):
 	var node: Node3D = get_child(i)
-	var step_height: float
+	node.position = Vector3()
+	var step_height = get_true_step_height()
 	var step_width = width
-	var step_depth: float
-	var step_calculation = calculation
-
-	if type == Type.RAMP:
-		step_calculation = Calculation.STEP_DIMENSIONS
-
-	match step_calculation:
-		Calculation.STAIRCASE_DIMENSIONS:
-			step_height = height / steps
-			step_depth = depth / steps
-		Calculation.STEP_DIMENSIONS:
-			step_height = height
-			step_depth = depth
+	var step_depth = get_true_step_depth()
+	var offset = get_anchor_offset(anchor)
 
 	match type:
 		# Filled with CSGBox3Ds
@@ -223,6 +303,9 @@ func refresh_step(i: int):
 				node.polygon[2] = Vector2(step_depth, step_height)
 				node.depth = step_width
 				node.position.x = -step_width / 2.0
+
+	# Apply anchor offset
+	node.position += offset
 
 func _ready():
 	set_steps(steps)
